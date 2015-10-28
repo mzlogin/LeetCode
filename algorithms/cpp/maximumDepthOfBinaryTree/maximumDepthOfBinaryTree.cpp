87740912324dd6a63a0cf5b0f77c6cb8ea2e5064
Zhuang Ma <ChumpMa@gmail.com>
2015-10-28 21:50:19 +0800

    switch file comments header style to style auto generated by mzlogin/code_complete

--- algorithms/cpp/maximumDepthOfBinaryTree/maximumDepthOfBinaryTree.cpp
+++ algorithms/cpp/maximumDepthOfBinaryTree/maximumDepthOfBinaryTree.cpp
@@ -1,15 +1,15 @@
-// Source : https://leetcode.com/problems/maximum-depth-of-binary-tree/
-// Author : Zhuang Ma
-// Date   : 2015-10-28
-
-/* Problem
- *
- * Given a binary tree, find its maximum depth.
- *
- * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
- */
+/**
+ * Source : https://leetcode.com/problems/maximum-depth-of-binary-tree/
+ * Author : Zhuang Ma
+ * Email  : chumpma(at)gmail.com
+ * Date   : 2015-10-28
+ * */
 
 /**
+ * Problem:
+ * Given a binary tree, find its maximum depth.
+ * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ *
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
