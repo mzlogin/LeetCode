e72cd3dc1fc135dc773928dec278bbfe66964f12
Zhuang Ma <chumpma@gmail.com>
2016-01-01 00:46:55 +0800

    add comments

--- algorithms/cpp/powerOfTwo/solution.h
+++ algorithms/cpp/powerOfTwo/solution.h
@@ -1,3 +1,15 @@
+/**
+ * Source : https://leetcode.com/problems/power-of-two/
+ * Author : Zhuang Ma
+ * Email  : chumpma(at)gmail.com
+ * Date   : 2016-01-01
+ * */
+
+/**
+ * Problem:
+ * Given an integer, write a function to determine if it is a power of two.
+ */
+
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
